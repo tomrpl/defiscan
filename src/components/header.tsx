56742da9923c258cac6340eb11608452163fa9dd
Yves Boutellier <63062257+yvesbou@users.noreply.github.com>
2024-10-25 19:57:38 +0200

    fixed header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import { AlignLeft, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import MobileNav from "@/components/mobile-nav";
 
 export default function Header() {
   const [isMobileOpen, setIsMobileOpen] = useState(false);
@@ -12,57 +9,27 @@ export default function Header() {
       <div className="container flex h-16 max-w-screen-2xl items-center justify-between">
         <div className="flex items-center space-x-3">
           <Link href="/" className="flex items-center space-x-3 text-primary">
-            <img src={'/images/logo.png'} width="146" height="50"/>
+            <img src={"/images/logo.png"} width="146" height="50" />
           </Link>
         </div>
         <div className="flex items-right space-x-5 md:space-x-6">
-        <Link
-          key="learn-more-nav"
-          href="/learn-more"
-          className="font-normal hover:text-primary transition-colors flex items-center text-primary"
-        >
-          <span>Learn more</span>
-        </Link>
-        <Button
-          variant="ghost"
-          className="p-0 text-primary hover:bg-transparent hover:text-primary md:hidden"
-          onClick={() => setIsMobileOpen(!isMobileOpen)}
-        >
-          <>
-            {isMobileOpen ? (
-              <X className="size-6" />
-            ) : (
-              <AlignLeft className="size-6" />
-            )}
-            <span className="sr-only">Menu</span>
-          </>
-        </Button>
-        <Link
-          key="submit-report-nav"
-          href="/submit-report"
-          className="font-normal hover:text-primary transition-colors flex items-center text-primary"
-        >
-          <span>Submit report</span>
-        </Link>
-        <Button
-          variant="ghost"
-          className="p-0 text-primary hover:bg-transparent hover:text-primary md:hidden"
-          onClick={() => setIsMobileOpen(!isMobileOpen)}
-        >
-          <>
-            {isMobileOpen ? (
-              <X className="size-6" />
-            ) : (
-              <AlignLeft className="size-6" />
-            )}
-            <span className="sr-only">Menu</span>
-          </>
-        </Button>
+          <Link
+            key="learn-more-nav"
+            href="/learn-more"
+            className="font-normal hover:text-primary transition-colors flex items-center text-primary"
+          >
+            <span>Learn more</span>
+          </Link>
+
+          <Link
+            key="submit-report-nav"
+            href="/submit-report"
+            className="font-normal hover:text-primary transition-colors flex items-center text-primary"
+          >
+            <span>Submit report</span>
+          </Link>
         </div>
       </div>
-      {isMobileOpen && (
-        <MobileNav onOpenChange={() => setIsMobileOpen(false)} />
-      )}
     </header>
   );
 }
